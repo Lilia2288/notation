7103d724079f84a1b950e587a84c0bf790767156
Lilia2288 <kubalila501@gmail.com>
2025-02-26 18:13:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,35 @@ def get_events():
     data = load_data()
     return data["events"]
 
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Привіт! я бот для управління нотатками "
+        "Опис команд: /event, /add_event, /list_events, /help."
+    )
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    help_text = (
+        "Команди:\n"
+        "/start - запуск бота"
+        "/help - виводе цей список"
+        "/add_event - <ім’я> <опис> додає нову нотатку"
+        "/list_events - покаже всі створенні нотатки"
+    )    
+    await update.message.reply_text(help_text)
+
+async def add_event_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_text = update.message.text.strip()
+    parts = message_text.split(" ", 2)
+    if len(parts) < 3:
+        await update.message.reply_text(
+            "використання: /add_event <назва> <опис> \n"
+            "Приклад: /add_event Вечірка о 20:00 у клубі"
+        )
+        return 
+    name = parts[1]
+    discription = parts[2]
+    add_event(name, discription)
+    await update.message.reply_text(
+        "✅ додана нова замітка"
+    )
+
